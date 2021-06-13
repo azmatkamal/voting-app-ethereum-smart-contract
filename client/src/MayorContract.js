2471db872ca9466d7b90a624de4d33200b99178b
Muhammad Azmat Kamal Khan <azmat.kamalkhan@gmail.com>
2021-06-14 01:01:09 +0200

    file change

--- client/src/MayorContract.js
+++ client/src/MayorContract.js
@@ -2,9 +2,7 @@ import Web3 from "web3";
 import truffleContract from "@truffle/contract";
 import MayorMultipleCandidates from "./contracts/MayorMultipleCandidates.json";
 
-// const Mayor = JSON.parse(MayorMultipleCandidates);
 const ganache_address = "http://127.0.0.1:8545";
-// const web3 = new Web3(ganache_address);
 const web3 = new Web3(new Web3.providers.HttpProvider(ganache_address));
 
 const accounts = async () => {
